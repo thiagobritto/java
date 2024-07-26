af8717dc054fd9a4efe978a793dca69224bda059
Thiago <thiagobritto.developer@gmail.com>
2024-07-26 11:34:27 -0300

    hello world

--- java2/src/basic/helloworld/Main.java
+++ java2/src/basic/helloworld/Main.java
@@ -1,9 +1,10 @@
-package java2;
+package basic.helloworld;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("ok");
+		
+		System.out.println("Hello, World!");
 
 	}
 
